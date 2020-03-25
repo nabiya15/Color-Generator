0253c39a3ee4d47468d944e75318bc69cd03b777
unknown <m.nabiya@gmail.com>
2020-03-24 17:59:18 -0700

    tiny update

--- script.js
+++ script.js
@@ -5,17 +5,13 @@ var body = document.getElementById("gradient");
 var randomColorBtn=document.getElementById("randomColor");
 
 window.onload=function(){
-	setGradient();
+	this.randomColor();
 }
 
 function setGradient() {
 	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
+	"linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	
 	css.textContent = body.style.background + ";";
 }
 
@@ -23,14 +19,9 @@ function randomColor(){
 	rColor1=  "#"+Math.floor((Math.random() * (9999999-1000000+1)+1000000)).toString(16);
 	rColor2=  "#"+Math.floor((Math.random() * (9999999-1000000+1)+1000000)).toString(16);
 	
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ rColor1
-	+ ", " 
-	+ rColor2
-	+ ")";
+	body.style.background = "linear-gradient(to right, " + rColor1 + ", " + rColor2+ ")";
 	css.textContent = body.style.background + ";";
-
+	
 	color1.value=rColor1;
 	color2.value=rColor2
 }
