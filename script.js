0c5974a70a7681060b3e798cf1447289f7c187ac
unknown <m.nabiya@gmail.com>
2020-03-24 17:53:55 -0700

    cool changes

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomColorBtn=document.getElementById("randomColor");
+
+window.onload=function(){
+	setGradient();
+}
 
 function setGradient() {
 	body.style.background = 
@@ -12,10 +17,26 @@ function setGradient() {
 	+ ")";
 
 	css.textContent = body.style.background + ";";
-	console.log(color1.value);
-	console.log(color2.value);
+}
+
+function randomColor(){
+	rColor1=  "#"+Math.floor((Math.random() * (9999999-1000000+1)+1000000)).toString(16);
+	rColor2=  "#"+Math.floor((Math.random() * (9999999-1000000+1)+1000000)).toString(16);
+	
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ rColor1
+	+ ", " 
+	+ rColor2
+	+ ")";
+	css.textContent = body.style.background + ";";
+
+	color1.value=rColor1;
+	color2.value=rColor2
 }
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+randomColorBtn.addEventListener("click",randomColor);
